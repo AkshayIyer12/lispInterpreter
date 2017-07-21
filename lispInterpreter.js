064c5fc414b978eedcc05d905c9f88638bd8861b
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-07-21 13:52:57 +0530

    Identation and comments checked and added where necessary

--- lispInterpreter.js
+++ lispInterpreter.js
@@ -17,8 +17,7 @@ const minNumber = (a, b) => a < b ? a : b
 const notNumber = (a) => !a
 const defLisp = (a, b) => { ENV[a] = b }
 const printLisp = (a) => { return a }
-// const ifLisp = (a, b, c) => a ? b : c
-// const beginLisp = (a) => { }
+
 
 const spaceParsedOp = (input) => {
   let pulsar = input.match(/^\s+/)
@@ -26,26 +25,30 @@ const spaceParsedOp = (input) => {
   let pulsarLength = pulsar[0].length
   return [null, input.slice(pulsarLength)]
 }
+
 /* Add, Subtract, Multiply and Divide */
 const plusParser = (data) => data.startsWith('+') ? [add, data.slice(1)] : null
 const minusParser = (data) => data.startsWith('-') ? [sub, data.slice(1)] : null
 const starParser = (data) => data.startsWith('*') ? [mul, data.slice(1)] : null
 const slashParser = (data) => data.startsWith('/') ? [div, data.slice(1)] : null
+
 /* >, >=, <, <=, == */
 const greaterThanParser = (data) => data.startsWith('>') ? [greaterThan, data.slice(1)] : null
 const lessThanParser = (data) => data.startsWith('<') ? [lessThan, data.slice(1)] : null
 const greaterThanEqualToParser = (data) => data.startsWith('>=') ? [greaterThanEqualTo, data.slice(2)] : null
 const lessThanEqualToParser = (data) => data.startsWith('<=') ? [lessThanEqualTo, data.slice(2)] : null
 const equalToParser = (data) => data.startsWith('==') ? [EqualTo, data.slice(2)] : null
+
 /* Max, Min and Not */
 const maxParser = (data) => data.startsWith('max') ? [maxNumber, data.slice(3)] : null
 const minParser = (data) => data.startsWith('min') ? [minNumber, data.slice(3)] : null
 const notParser = (data) => data.startsWith('not') ? [notNumber, data.slice(3)] : null
+
 /* Define, if and Begin */
 const defParser = (data) => data.startsWith('define') ? [defLisp, data.slice(6)] : null
-// const ifParser = (data) => data.startsWith('if') ? [ifLisp, input.slice(2)] : null
 const printedParser = (data) => data.startsWith('print') ? [printLisp, data.slice(5)] : null
-// const beginparser = (data) => data.startsWith('begin') ? [beginLisp, input.slice(5)] : null
+
+/* Open and Close Bracket */ 
 const openBracketOp = (input) => (input.startsWith('(')) ? ['(', input.slice(1)] : null
 const closeBracketOp = (input) => (input.startsWith(')')) ? [')', input.slice(1)] : null
 
