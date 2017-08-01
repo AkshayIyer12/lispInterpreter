5b7e0ae69b5177ad53fe8be88bf8db86b3c0ad15
Akshay Iyer <c.m.akshay.iyer@gmail.com>
2017-08-01 14:38:19 +0530

    Change variable names

--- lispInterpreter_v3.js
+++ lispInterpreter_v3.js
@@ -1,10 +1,11 @@
 var input = require('fs').readFileSync('newexample.txt', 'utf-8')
 const ENV = {}
 let mat, regexp = /^[0-9]+/, output
-const spaceParser = (input) => (mat = input.match(/^[\s\t\n]+/)) ? [mat[0], input.slice(mat[0].length)] : null
-const identifierParser = (input) => (mat = input.match(/^[a-z]+[0-9]*[a-z]*/i)) ? [mat[0], input.slice(mat[0].length)] : null
-const numberParser = (input) => (mat = regexp.exec(input)) ? [parseInt(mat[0]), input.slice(mat[0].length)] : null
-const stringParser = (data) => {
+
+const skipSpaces = (input) => (mat = input.match(/^[\s\t\n]+/)) ? [mat[0], input.slice(mat[0].length)] : null
+const findIdentifier = (input) => (mat = input.match(/^[a-z]+[0-9]*[a-z]*/i)) ? [mat[0], input.slice(mat[0].length)] : null
+const findNumber = (input) => (mat = regexp.exec(input)) ? [parseInt(mat[0]), input.slice(mat[0].length)] : null
+const findString = (data) => {
   if (data[0] === '"') {
     data = data.substring(1, data.length)
     let i = data.search('"')
@@ -12,55 +13,60 @@ const stringParser = (data) => {
   }
   return null
 }
+
 const add = input => input.reduce((accum, value) => (ENV[value]) ? accum + ENV[value] : accum + value, 0)
 const sub = input => input.reduce((accum, value) => (ENV[value]) ? accum - ENV[value] : accum - value)
 const mul = input => input.reduce((accum, value) => (ENV[value]) ? accum * ENV[value] : accum * value, 1)
 const div = input => input.reduce((accum, value) => (ENV[value]) ? accum / ENV[value] : accum / value)
-const greaterThan = (a, b) => a > b
-const lessThan = (a, b) => a < b
-const greaterThanEqualTo = (a, b) => a >= b
-const lessThanEqualTo = (a, b) => a <= b
-const EqualTo = (a, b) => a === b
-const notNumber = (a) => !a
-const maxNumber = (a, b) => a > b ? a : b
-const minNumber = (a, b) => a < b ? a : b
+const gt = (a, b) => a > b
+const lt = (a, b) => a < b
+const gteq = (a, b) => a >= b
+const lteq = (a, b) => a <= b
+const eq = (a, b) => a === b
+const not = (a) => !a
+const max = (a, b) => a > b ? a : b
+const min = (a, b) => a < b ? a : b
 const listCall = (a) => { return { type: 'list', args: a } }
-const listCar = (a) => a[0].shift()
-const listCdr = (a) => {
+const car = (a) => a[0].shift()
+const cdr = (a) => {
   a[0].shift()
   return a[0]
 }
-const listCons = (a) => {
+const cons = (a) => {
   a[1].unshift(a[0])
   return a[1]
 }
-const isListLisp = (a) => !!(Array.isArray(a))
-const defineLisp = (a, b) => { ENV[a] = b }
-const printLisp = (a) => (ENV[a]) ? console.log(ENV[a]) : a
-const ifLisp = (a, b, c) => a ? b : c
-const plusParser = (data) => data.startsWith('+') ? [add, data.slice(1)] : null
-const minusParser = (data) => data.startsWith('-') ? [sub, data.slice(1)] : null
-const mulParser = (data) => data.startsWith('*') ? [mul, data.slice(1)] : null
-const divParser = (data) => data.startsWith('/') ? [div, data.slice(1)] : null
-const greaterThanParser = (data) => data.startsWith('>') ? [greaterThan, data.slice(1)] : null
-const lessThanParser = (data) => data.startsWith('<') ? [lessThan, data.slice(1)] : null
-const greaterThanEqualToParser = (data) => data.startsWith('>=') ? [greaterThanEqualTo, data.slice(2)] : null
-const lessThanEqualToParser = (data) => data.startsWith('<=') ? [lessThanEqualTo, data.slice(2)] : null
-const equalToParser = (data) => data.startsWith('==') ? [EqualTo, data.slice(2)] : null
-const maxParser = (data) => data.startsWith('max') ? [maxNumber, data.slice(3)] : null
-const minParser = (data) => data.startsWith('min') ? [minNumber, data.slice(3)] : null
-const notParser = (data) => data.startsWith('not') ? [notNumber, data.slice(3)] : null
-const listParser = (data) => data.startsWith('list') ? [listCall, data.slice(4)] : null
-const carList = (data) => data.startsWith('car') ? [listCar, data.slice(3)] : null
-const cdrList = (data) => data.startsWith('cdr') ? [listCdr, data.slice(3)] : null
-const consList = (data) => data.startsWith('cons') ? [listCons, data.slice(4)] : null
-const isListIden = (data) => data.startsWith('isList') ? [isListLisp, data.slice(6)] : null
-const defineSlicerParser = (data) => data.startsWith('define') ? [defineLisp, data.slice(6)] : null
-const ifSlicerParser = (data) => data.startsWith('if') ? [ifLisp, data.slice(2)] : null
-const printSlicerParser = (data) => data.startsWith('print') ? [printLisp, data.slice(5)] : null
-const lambdaSlicerParser = (data) => data.startsWith('lambda') ? ['lambda', data.slice(6)] : null
-const openBracket = (input) => (input.startsWith('(')) ? ['(', input.slice(1)] : null
-const closeBracket = (input) => (input.startsWith(')')) ? [')', input.slice(1)] : null
+const isList = (a) => !!(Array.isArray(a))
+const operationDefine = (a, b) => { ENV[a] = b }
+const operationPrint = (a) => (ENV[a]) ? console.log(ENV[a]) : a
+const operationIf = (a, b, c) => a ? b : c
+
+const findPlus = (data) => data.startsWith('+') ? [add, data.slice(1)] : null
+const findMinus = (data) => data.startsWith('-') ? [sub, data.slice(1)] : null
+const findMult = (data) => data.startsWith('*') ? [mul, data.slice(1)] : null
+const findDiv = (data) => data.startsWith('/') ? [div, data.slice(1)] : null
+const findGT = (data) => data.startsWith('>') ? [gt, data.slice(1)] : null
+const findLT = (data) => data.startsWith('<') ? [lt, data.slice(1)] : null
+const findGTEQ = (data) => data.startsWith('>=') ? [gteq, data.slice(2)] : null
+const findLTEQ = (data) => data.startsWith('<=') ? [lteq, data.slice(2)] : null
+const findEQ = (data) => data.startsWith('==') ? [eq, data.slice(2)] : null
+const findMax = (data) => data.startsWith('max') ? [max, data.slice(3)] : null
+const findMin = (data) => data.startsWith('min') ? [min, data.slice(3)] : null
+const findNot = (data) => data.startsWith('not') ? [not, data.slice(3)] : null
+
+const findList = (data) => data.startsWith('list') ? [listCall, data.slice(4)] : null
+const findCar = (data) => data.startsWith('car') ? [car, data.slice(3)] : null
+const findCdr = (data) => data.startsWith('cdr') ? [cdr, data.slice(3)] : null
+const findCons = (data) => data.startsWith('cons') ? [cons, data.slice(4)] : null
+const findIsList = (data) => data.startsWith('isList') ? [isList, data.slice(6)] : null
+
+const findDefine = (data) => data.startsWith('define') ? [operationDefine, data.slice(6)] : null
+const findIf = (data) => data.startsWith('if') ? [operationIf, data.slice(2)] : null
+const findPrint = (data) => data.startsWith('print') ? [operationPrint, data.slice(5)] : null
+const findLambda = (data) => data.startsWith('lambda') ? ['lambda', data.slice(6)] : null
+const findOpenBracket = (input) => (input.startsWith('(')) ? ['(', input.slice(1)] : null
+const findCloseBracket = (input) => (input.startsWith(')')) ? [')', input.slice(1)] : null
+
 const parserFactory = (...parsers) => (input) => {
   for (let parser of parsers) {
     let output = parser(input)
@@ -68,6 +74,7 @@ const parserFactory = (...parsers) => (input) => {
   }
   return null
 }
+
 const allParser = (...parsers) => (input) => {
   let result = []
   for (let parser of parsers) {
@@ -78,11 +85,13 @@ const allParser = (...parsers) => (input) => {
   }
   return [result, input]
 }
-const operatorFinder = (input) => parserFactory(plusParser, minusParser, mulParser,
-  divParser, greaterThanEqualToParser, lessThanEqualToParser, equalToParser,
-  greaterThanParser, lessThanParser, maxParser, minParser, notParser, listParser,
-  carList, cdrList, consList, isListIden)(input)
-const operatorParser = (input) => {
+
+const findOperator = (input) => parserFactory(findPlus, findMinus, findMult,
+  findDiv, findGTEQ, findLTEQ, findEQ,
+  findGT, findLT, findMax, findMin, findNot, findList,
+  findCar, findCdr, findCons, findIsList)(input)
+
+const parseOperators = (input) => {
   let count = 1
   if (!input.startsWith('(')) return null
   input = input.slice(1)
@@ -92,49 +101,52 @@ const operatorParser = (input) => {
     input.startsWith('min')) {
     count++
   }
-  let output = operatorFinder(input)
+  let output = findOperator(input)
   if (output === null) return null
-  // For Operator Operand1 Operand2
   let result = [], vid = '', args = 'args'
   result.push(output[0])
   while (output[0] !== ')') {
-    output = spaceParser(output[1])
-    output = expressionParser(output[1])
+    output = skipSpaces(output[1])
+    output = parseExpression(output[1])
     if (ENV[output[0]]) {
       (ENV[output[0]].args) ? result.push(ENV[output[0]].args) : result.push(ENV[output[0]])
     } else {
       result.push(output[0])
     }
-    output = (vid = closeBracket(output[1])) ? vid : output
+    output = (vid = findCloseBracket(output[1])) ? vid : output
   }
   if (output[0] === ')') {
     if (output[1] === '') {
-      return [evaluate(result, count)]
+      return [applyFunction(result, count)]
     } else {
-      return [evaluate(result, count), output[1]]
+      return [applyFunction(result, count), output[1]]
     }
   }
 }
-const evaluate = (input, count) => {
+
+const applyFunction = (input, count) => {
   let operation = input.shift()
   if (count > 1) return operation(...input)
   else return operation(input)
 }
-const expressionParser = (input) => parserFactory(numberParser, stringParser,
-  lambdaParser, identifierParser, operatorParser)(input)
-const lambdaArgumentsParser = (input) => {
+
+const parseExpression = (input) => parserFactory(findNumber, findString,
+  parseLambda, findIdentifier, parseOperators)(input)
+
+const findLambdaArguments = (input) => {
   let result = [], output
-  input = openBracket(input)
+  input = findOpenBracket(input)
   input = input[1]
   while (!input.startsWith(')')) {
-    input = identifierParser(input)
+    input = findIdentifier(input)
     result.push(input[0])
-    input = (output = spaceParser(input[1])) ? output[1] : input[1]
+    input = (output = skipSpaces(input[1])) ? output[1] : input[1]
   }
-  input = closeBracket(input)
+  input = findCloseBracket(input)
   return [result, input[1]]
 }
-const lambdaBodyParser = (input) => {
+
+const findLambdaBody = (input) => {
   let count = 0, k = 0, output = ''
   do {
     if (input[k] === '(') count++
@@ -145,15 +157,17 @@ const lambdaBodyParser = (input) => {
   input = input.slice(k)
   return [output, input]
 }
+
 const defineLambda = (input) => {
   let obj = {}, count = 3
-  let output = allParser(openBracket, lambdaSlicerParser, spaceParser, lambdaArgumentsParser,
-    spaceParser, lambdaBodyParser, closeBracket)(input)
+  let output = allParser(findOpenBracket, findLambda, skipSpaces, findLambdaArguments,
+    skipSpaces, findLambdaBody, findCloseBracket)(input)
   if (output === null) return null
   let [[, Type, , Args, , Body], rest] = output
   obj.type = Type, obj.args = Args, obj.body = Body
   return [obj, rest, count]
 }
+
 const checker = (output) => {
   if ((output[0] === 'list') || (output[0] === 'max') || (output[0] === 'min') ||
    (output[0] === 'car') || (output[0] === 'cdr') || (output[0] === 'cons') ||
@@ -161,28 +175,29 @@ const checker = (output) => {
     return null
   }
 }
-const lambdaParser = (input) => {
+
+const parseLambda = (input) => {
   if (!input.startsWith('(')) return null
   input = input.slice(1)
-  let output = identifierParser(input)
-    let type = 'type'
+  let output = findIdentifier(input)
+  let type = 'type'
   if ((output === null) || (checker(output) === null) || (ENV[output[0]].type === undefined)) return null
   let key = output[0], arr = []
   while (!output[1].startsWith(')')) {
-    output = spaceParser(output[1])
-    if (numberParser(output[1])) {
-      output = numberParser(output[1])
+    output = skipSpaces(output[1])
+    if (findNumber(output[1])) {
+      output = findNumber(output[1])
       arr.push(output[0])
     }
-    if (identifierParser(output[1])) {
-      output = identifierParser(output[1])
+    if (findIdentifier(output[1])) {
+      output = findIdentifier(output[1])
       if (ENV[output[0]] !== undefined) {
         output[0] = ENV[output[0]]
         arr.push(output[0])
       }
     }
     if (output[1].startsWith('(')) {
-      output = lambdaParser(output[1])
+      output = parseLambda(output[1])
       arr.push(output[0][0])
     }
   }
@@ -196,57 +211,59 @@ const lambdaParser = (input) => {
   }
   env[body] = env[body].replace(ENV[key].args[0], value)
   env[body] = env[body].replace(ENV[key].args[0], value)
-  output = (vid = closeBracket(output[1])) ? vid : output
+  output = (vid = findCloseBracket(output[1])) ? vid : output
   if (output[0] === ')') {
-    return [operatorParser(env[body]), output[1]]
+    return [parseOperators(env[body]), output[1]]
   }
 }
-const defineParser = (input) => {
+
+const parseDefine = (input) => {
   let arr = [], count = 1
-  let output = allParser(openBracket, defineSlicerParser, spaceParser, identifierParser,
-    spaceParser)(input)
+  let output = allParser(findOpenBracket, findDefine, skipSpaces, findIdentifier,
+    skipSpaces)(input)
   if (output === null) return null
   let [[ , defineFunc, , iden], rest] = output
   input = rest
-  output = defineLambda(input)
-  if (output === null) output = expressionParser(input)
+  output = (output = defineLambda(input)) ? output : parseExpression(input)
+  // if (output === null) output = parseExpression(input)
   let [val1, val2] = output
   arr.push(defineFunc, iden, val1)
-  count += 3
-  evaluate(arr, count)
-  input = closeBracket(val2)
+  applyFunction(arr, count + 3)
+  input = findCloseBracket(val2)
   return input[1]
 }
-const printParser = (input) => {
+
+const parsePrint = (input) => {
   let arr = [], count = 1
-  let output = allParser(openBracket, printSlicerParser, spaceParser, expressionParser,
-    closeBracket)(input)
+  let output = allParser(findOpenBracket, findPrint, skipSpaces, parseExpression,
+    findCloseBracket)(input)
   if (output === null) return null
   let [[, printFunc, , exp], rest] = output
   arr.push(printFunc)
   ENV[exp] ? arr.push(ENV[exp]) : arr.push(exp)
-  console.log('Evaluate to ' + evaluate(arr, count))
-  if (rest === null) {
-    return null
-  }
-  return rest
+  console.log('Evaluate to ' + applyFunction(arr, count))
+  return (rest === null) ? null : rest
 }
-const ifParser = (input) => {
+
+const parseIf = (input) => {
   let arr = [], count = 0
-  let output = allParser(openBracket, ifSlicerParser, spaceParser, expressionParser, spaceParser, expressionParser, spaceParser, expressionParser, closeBracket)(input)
+  let output = allParser(findOpenBracket, findIf, skipSpaces, parseExpression, skipSpaces, parseExpression, skipSpaces, parseExpression, findCloseBracket)(input)
   if (output === null) return null
   let [[, ifFun, , test, , conseq, , alt], rest] = output
   arr.push(ifFun, test, conseq, alt)
-  console.log('Evaluate to ' + evaluate(arr, count + 3))
+  console.log('Evaluate to ' + applyFunction(arr, count + 3))
   return rest
 }
-const statementParser = (input) => parserFactory(defineParser, printParser, ifParser)(input)
-const programParser = (input) => {
+
+const parseStatement = (input) => parserFactory(parseDefine, parsePrint, parseIf)(input)
+
+const parseProgram = (input) => {
   while (input !== '' && input !== null) {
     let spaceParsed = ''
-    input = (spaceParsed = spaceParser(input)) ? spaceParsed[1] : input
-    input = statementParser(input)
+    input = (spaceParsed = skipSpaces(input)) ? spaceParsed[1] : input
+    input = parseStatement(input)
   }
   return ENV
 }
-console.log((output = programParser(input)) ? output : 'Error')
+
+console.log((output = parseProgram(input)) ? output : 'Error')
